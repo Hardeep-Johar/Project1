1b4f4241cf9f4889077d3132284c6807150c52aa
Hardeep-Johar <hardeep.johar@gmail.com>
2023-01-01 18:19:29 -0500

    test2

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 # exit on error
 set -o errexit
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 python manage.py migrate
 #echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin1', 'admin@example.com', 'pass')" | python manage.py shell
