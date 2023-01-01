38498ac61463bf5fa3c294127b2e83f4a7a87951
Hardeep-Johar <hardeep.johar@gmail.com>
2023-01-01 18:17:45 -0500

    test2

--- Project1/settings.py
+++ Project1/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    #"django.contrib.staticfiles",
     'app1.apps.App1Config',
 ]
 if LOCAL:
