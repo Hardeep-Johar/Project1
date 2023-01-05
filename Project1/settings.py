01312e09ab02935a8dcf9af213a17743e395092c
Hardeep-Johar <hardeep.johar@gmail.com>
2023-01-05 13:00:26 -0500

    test2

--- Project1/settings.py
+++ Project1/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-LOCAL = False
+LOCAL = True
 from pathlib import Path
 import os
 import dj_database_url
@@ -50,8 +50,8 @@ INSTALLED_APPS = [
     #"django.contrib.staticfiles",
     'app1.apps.App1Config',
 ]
-if LOCAL:
 
+if LOCAL:
     MIDDLEWARE = [
         "django.middleware.security.SecurityMiddleware",
         "django.contrib.sessions.middleware.SessionMiddleware",
@@ -97,17 +97,12 @@ WSGI_APPLICATION = "Project1.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if LOCAL:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/webapps1',
-                                          conn_max_age=600)}
+}
 
     # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Holding(models.Model):
+    iso = models.CharField(max_length=3)
+    local_currency_value = models.FloatField(default=0.0)
+    buy_date = models.DateField()
